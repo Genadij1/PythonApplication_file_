8682d35c99e67eaa1b25350a24ae6a0a188e823f
Genacud <bgennadij265@icloud.com>
2023-04-23 10:28:27 +0300

    updata

--- PythonApplication_file_.py
+++ PythonApplication_file_.py
@@ -1,9 +1,1 @@
-try:
-    _file = open('New file.txt', 'r')
-    _file.readline()
-    if _file.readline() == 0:
-        print('File is not empty')
-except Exception as e:
-    print(e)
-finally:    
-    _file.close()
+
