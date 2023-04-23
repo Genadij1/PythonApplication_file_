785bd42898a5949933481f82d5df6873c866b2d9
Genacud <bgennadij265@icloud.com>
2023-04-23 11:44:24 +0300

    ex_2-done

--- PythonApplication_file_.py
+++ PythonApplication_file_.py
@@ -1,1 +1,15 @@
+﻿
+try:
+    with open('New file.txt', 'r') as f:
+        line_number = int(input('Enter line number: ')) 
+        current_line_number = 1
+        for line in f:
+            if current_line_number == line_number:
+                print(line)
+                break
+        else:
+            print(f"Рядок з номером {line_number} не знайдено")
+except Exception as e:
+    print(e)
+
 
