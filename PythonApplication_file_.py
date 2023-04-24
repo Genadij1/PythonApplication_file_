65588ace97c45b6b45ee6f2b297ad29e7cbb9a7d
Genacud <bgennadij265@icloud.com>
2023-04-24 20:17:19 +0300

    ex_4-done

--- PythonApplication_file_.py
+++ PythonApplication_file_.py
@@ -1,1 +1,19 @@
+#word = input('Enter a word: ')
+#with open('New file.txt', 'r') as file:
+ #   line = file.readlines()
+ #   if word in line:
+  #      print(line)
+
+word = input('Enter word: ')
+ 
+inp = iter(open('New file.txt').readlines())
+ 
+for i in inp:
+    if word in i:
+        print(i)
+        
+
+
+
+
 
