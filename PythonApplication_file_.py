49363dbe646c1b442fecb0bc732a7358c91c5456
Genacud <bgennadij265@icloud.com>
2023-04-28 13:03:53 +0300

    ex_10-done

--- PythonApplication_file_.py
+++ PythonApplication_file_.py
@@ -1,1 +1,12 @@
 
+with open('file.txt', 'r') as f:
+    content = f.read()
+    lines = content.split('\n')
+    print(content)
+    delete = int(input('delete line: '))
+    del lines[delete - 1]
+
+with open('file.txt', 'w') as f:
+    f.write('\n'.join(lines))
+    print('\n'.join(lines))
+            
