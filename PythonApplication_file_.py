e434179c89c9dbce0e27d0d8e45aacc0ba2f8b05
Genacud <bgennadij265@icloud.com>
2023-04-28 10:27:20 +0300

    ex_6-done

--- PythonApplication_file_.py
+++ PythonApplication_file_.py
@@ -1,1 +1,14 @@
 
+try:
+    with open('file.txt', 'r') as file:
+        text = file.readlines()
+        start = int(input('Enter the line with start: '))
+        for line in text:
+            if start <= 0:
+                print('Enter the correct line!')
+            else:
+                print(' '.join(text[start-1:]))
+                break
+except ValueError as e: 
+    print(f'error: {e}')
+      
