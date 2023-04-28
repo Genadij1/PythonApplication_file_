fd927145567b7fcb5c4c7b4887098e094ea3459c
Genacud <bgennadij265@icloud.com>
2023-04-28 12:10:34 +0300

    ex_8-done

--- PythonApplication_file_.py
+++ PythonApplication_file_.py
@@ -1,1 +1,7 @@
 
+with open('file.txt', 'r') as f:
+    content = f.readlines()
+    if len(content) == len(set(content)):
+        print('Повторів рядків немає')
+    else:
+        print('Повтори є')
