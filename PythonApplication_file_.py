9652e3524a751c994e0d6af13794589cbd7c6d30
Genacud <bgennadij265@icloud.com>
2023-04-28 11:09:16 +0300

    ex_7-done

--- PythonApplication_file_.py
+++ PythonApplication_file_.py
@@ -1,1 +1,11 @@
 
+file = open('file.txt', 'r')
+line = file.readlines()
+print(line)
+line_change = int(input('Enter the line you want to change: ')) 
+text = input('Enter the text you want to change: ')
+for el in line:
+        if el == line[line_change - 1]:
+            line[line_change - 1] = text + '\n'
+            print(' '.join(line))
+file.close()
